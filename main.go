b2e3ad0d672e3bbe8bda7a0521f16da6a84798e4
Maxim Lebedev <git@toby3d.me>
2023-09-30 20:19:02 +0600

    :technologist: Added profiling flags support

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"flag"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
+	"syscall"
 
 	"github.com/caarlos0/env/v9"
 
@@ -19,7 +26,13 @@ var (
 	logger = log.New(os.Stdout, "Micropub\t", log.LstdFlags)
 )
 
+var cpuProfilePath, memProfilePath string
+
 func init() {
+	flag.StringVar(&cpuProfilePath, "cpuprofile", "", "set path to saving CPU memory profile")
+	flag.StringVar(&memProfilePath, "memprofile", "", "set path to saving pprof memory profile")
+	flag.Parse()
+
 	if err := env.ParseWithOptions(config, env.Options{
 		Prefix: "MICROPUB_",
 	}); err != nil {
@@ -28,6 +41,7 @@ func init() {
 }
 
 func main() {
+	ctx := context.Background()
 	mediaRepo := mediamemoryrepo.NewMemoryMediaRepository()
 	mediaUseCase := mediaucase.NewMediaUseCase(mediaRepo)
 	mediaHandler := mediahttpdelivery.NewHandler(mediaUseCase, *config)
@@ -47,7 +61,48 @@ func main() {
 		}),
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		logger.Fatal("cannot listen and serve:", err)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	if cpuProfilePath != "" {
+		cpuProfile, err := os.Create(cpuProfilePath)
+		if err != nil {
+			logger.Fatalln("could not create CPU profile:", err)
+		}
+		defer cpuProfile.Close()
+
+		if err = pprof.StartCPUProfile(cpuProfile); err != nil {
+			logger.Fatalln("could not start CPU profile:", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	go func() {
+		logger.Printf("started at %s, available at %s", config.HTTP.Bind, config.HTTP.BaseURL())
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Fatalln("cannot listen and serve:", err)
+		}
+	}()
+
+	<-done
+
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Fatalln("failed shutdown of server:", err)
+	}
+
+	if memProfilePath == "" {
+		return
+	}
+
+	memProfile, err := os.Create(memProfilePath)
+	if err != nil {
+		logger.Fatalln("could not create memory profile:", err)
+	}
+	defer memProfile.Close()
+
+	runtime.GC() // NOTE(toby3d): get up-to-date statistics
+
+	if err = pprof.WriteHeapProfile(memProfile); err != nil {
+		logger.Fatalln("could not write memory profile:", err)
 	}
 }
